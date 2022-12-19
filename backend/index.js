080ae0bc337a1623ef9d8d396c1cf5c61cfbdab7
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2022-12-19 15:30:28 -0600

    now the server serves the website, modifications to the package.json

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ const uploadFile = require('./middleware/multer');
 const app = express();
 const port = 5050;
 
+
 app.post('/upload',uploadFile(), function (req, res) {
     res.send('File uploaded successfully');
 });
 
-app.listen(port, ip, () => {
+app.listen(port, () => {
     console.log(`Servidor funcionando en el puerto: ${port}.`);
 });
+
+app.use(express.static('./build'));
