f409f320acca7e00f2a6ab914ff5e52476539ebd
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2022-12-20 17:05:46 -0600

    Squashed commit of the following:
    commit 791a0fcccb16087a47ba18a08529074e644f7f95
    Merge: 43c05e3 b074cb9
    Author: Erick Devv <111036821+ErickDevv@users.noreply.github.com>
    Date:   Tue Dec 20 15:58:18 2022 -0600
    
        Merge pull request #4 from ErickDevv/db
    
        Merge pull request #3 from ErickDevv/main
    
    commit b074cb9223a098ef6d8cbc03b1a843c39848d4db
    Merge: 0558ef6 43c05e3
    Author: Erick Devv <111036821+ErickDevv@users.noreply.github.com>
    Date:   Tue Dec 20 15:57:13 2022 -0600
    
        Merge pull request #3 from ErickDevv/main
    
        Updating the db branch to the latest changes
    
    commit 43c05e3fa48b4003f5e0f13653ecc839f258f391
    Merge: 02bced9 5d50913
    Author: Erick Devv <111036821+ErickDevv@users.noreply.github.com>
    Date:   Tue Dec 20 15:34:55 2022 -0600
    
        Merge branch 'main' of https://github.com/ErickDevv/cloudGum
    
    commit 02bced97e5c358c537b709ad504c38b5b91d0caf
    Author: Erick Devv <111036821+ErickDevv@users.noreply.github.com>
    Date:   Tue Dec 20 15:34:52 2022 -0600
    
        updated branch
    
    commit 5d50913c7aabc33cf348afa0671b95932ef71fd4
    Merge: db37ce3 4c5b0e9
    Author: Erick Devv <111036821+ErickDevv@users.noreply.github.com>
    Date:   Tue Dec 20 15:30:41 2022 -0600
    
        Merge pull request #2 from ErickDevv/frontend
    
        The frontend is now made with react

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const uploadFile = require('./middleware/multer');
+const cors = require('cors');
 
 const app = express();
 const port = 5050;
 
+app.use(express.json());
+
+app.use(cors({origin: '*'}));
 
 app.post('/upload',uploadFile(), function (req, res) {
-    res.send('File uploaded successfully');
+    if (!req.file) {
+        res.send('No file uploaded');
+    }else if (req.file) {
+        res.send('File uploaded successfully');
+    }
 });
 
 app.listen(port, () => {
